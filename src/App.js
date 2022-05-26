8928270ffc38921679c85952ffb7f806ac840b13
Steph <sweetcactusflower@gmail.com>
2022-05-25 21:07:21 -0400

    Added Search.jsx w/ layout and styling.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Destinations from './components/Destinations';
+import Search from './components/Search';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <Destinations />
+      <Search />
     </div>
   );
 }
